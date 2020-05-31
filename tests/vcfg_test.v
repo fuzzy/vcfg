0eb173dd38426c6a10e3c447fcc343009deb485a
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2020-05-31 04:42:29 -0700

    added test data and test for interpolated string values

--- tests/vcfg_test.v
+++ tests/vcfg_test.v
@@ -1,13 +1,22 @@
 import fuzzy.vcfg
 
-fn test_global_string_value() {
-  cfg := vcfg.new_parser('example.ini', false, false)
+fn data_setup() &vcfg.Vcfg {
+  cfg := vcfg.new_parser('example.ini', true, true)
   cfg.parse()
+  return cfg
+}
+
+fn test_global_string_value() {
+  cfg := data_setup()
   assert cfg.data['global']['test'] == 'test'
 }
 
 fn test_section_string_value() {
-  cfg := vcfg.new_parser('example.ini', false, false)
-  cfg.parse()
+  cfg := data_setup()
   assert cfg.data['test']['test'] == 'test'
+}
+
+fn test_section_string_interpolated_value() {
+  cfg := data_setup()
+  assert cfg.data['test']['interpolation'] == 'test'
 }
