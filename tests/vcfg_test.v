6c57cec908f3241b47e8cb518b61f6acab591149
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2020-05-31 04:04:32 -0700

    added section string value test and data to example ini

--- tests/vcfg_test.v
+++ tests/vcfg_test.v
@@ -5,3 +5,9 @@ fn test_global_string_value() {
   cfg.parse()
   assert cfg.data['global']['test'] == 'test'
 }
+
+fn test_section_string_value() {
+  cfg := vcfg.new_parser('example.ini', false, false)
+  cfg.parse()
+  assert cfg.data['test']['test'] == 'test'
+}
